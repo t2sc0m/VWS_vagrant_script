828305bfb77bb40b673c0deba7c601bf0b59338b
tescom <tescom@tescom.local>
2022-02-27 20:37:52 +0900

    .ssh 퍼미션 변경

--- SHELL/init.sh
+++ SHELL/init.sh
@@ -29,7 +29,7 @@ echo "root유저의 환경설정을 합니다."
 echo "ssh 설정을 등록합니다."
 sudo cp -rfp ${SSH} /root/.ssh
 sudo chown root:root /root/.ssh -R
-sudo chmod 755 /root/.ssh -R
+sudo chmod 644 /root/.ssh -R
 sudo chmod 600 /root/.ssh/id_rsa
 sudo chmod 644 /root/.ssh/authorized_keys
 
